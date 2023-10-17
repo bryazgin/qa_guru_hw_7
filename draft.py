8a68c663fda87a478a5b9ba08aac0843d9fef0b2
sergeybryazgin <sergeybryazgin@omg>
2023-10-17 23:21:50 +0300

    доделал тхт, почти пдф

--- draft.py
+++ draft.py
@@ -1,12 +1,24 @@
 import os, pytest
 from zipfile import ZipFile
+from pypdf import PdfReader
 
+#проверка txt
 with ZipFile('/Users/sergeybryazgin/PycharmProjects/qa_guru_hw_7/tests/tmp/test_file.zip', mode='r') as zf:
     with zf.open('book.txt', 'r') as txt_file:
-        #print(txt_file.read().decode('utf-8'))
         f = open(os.path.abspath('tests/resources/book.txt'), 'r')
-        #print(f.read())
-        assert f.read() == txt_file.read().decode('utf-8')
+        assert (txt_file.read().decode('utf-8')) == f.read()
 
-#f.close()
-#txt_file.close()
+
+#проверка xls
+
+
+#проверка pdf
+
+with ZipFile('/Users/sergeybryazgin/PycharmProjects/qa_guru_hw_7/tests/tmp/test_file.zip', mode='r') as zf:
+    with zf.open('SQL.pdf', 'r') as pdf_file:
+        reader = PdfReader('/Users/sergeybryazgin/PycharmProjects/qa_guru_hw_7/tests/resources/SQL.pdf')
+        for page in range(len(reader.pages)):
+            content = reader.pages[page]
+            print(content.extract_text())
+        assert (pdf_file.read().decode('utf-8')) == f.read()
+
